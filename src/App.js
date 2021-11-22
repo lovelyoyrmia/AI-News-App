2e3d512d35fdb3186656af087aaf817387d8ac99
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-11-22 10:55:32 +0700

    add footer css

--- src/App.js
+++ src/App.js
@@ -15,16 +15,32 @@ function App() {
         style={{
           display: "flex",
           textAlign: "center",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           height: "10vh",
           borderTop: "1px solid grey",
-          marginTop: "1rem",
+          marginTop: "0.5rem",
         }}
       >
-        <p>
-          Created by Lovelyo Yeremia <i className="fas fa-facebook" />
-        </p>
+        <p>Created by Lovelyo Yeremia</p>
+        <div className="footer">
+          <a
+            className="fab fa-github"
+            href="https://github.com/lovelyoyrmia"
+            target="_blank"
+          />
+          <a
+            className="fab fa-instagram"
+            href="https://instagram.com/lovelyoyrmia"
+            target="_blank"
+          />
+          <a
+            className="fab fa-linkedin"
+            href="https://www.linkedin.com/in/lovelyo-mokalu-a831aa175/"
+            target="_blank"
+          />
+        </div>
       </div>
     </>
   );
