1a7373cc5ffaff4a2e4d03b3e78c838da0cdef99
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-11-22 10:39:44 +0700

    remove how to use page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,2 @@
 export { default as Home } from "./Home/Home";
 export { default as Error } from "./Error/Error";
-export {
-  LatestNews,
-  NewsByCategories,
-  NewsBySources,
-  NewsByTerms,
-} from "./HowToUse";
